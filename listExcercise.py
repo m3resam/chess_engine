babb46b1804cfdafe37aa8750819f6b8bbeca7cb
Samuele Mereghetti <168123480+samuelemereghetti@users.noreply.github.com>
2024-05-18 21:16:08 +0000

    Added code description

--- listExcercise.py
+++ listExcercise.py
@@ -1,6 +1,8 @@
 from random import *
 
+# Create a list
 n = []
+
 '''
 print('Enter a positive int: ')
 N = int(input())
@@ -11,11 +13,12 @@ while N > 0:
     N -= 1
 '''
 
+# Loop 10 times and append a random number between 1 and 100
 for i in range(10):
     m = randint(1,100)
     n.append(m)
     
-
+# Look for the highest numer in the list and it's position
 max = n[0]
 pos = 0
 for i in n:
@@ -24,6 +27,7 @@ for i in n:
         max = i
         posd = pos
 
+# Print results
 print(n)
 print(max)
 print(posd)
