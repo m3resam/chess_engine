37b57675732acc52afbd5d1ffd938587ddb67021
Samuele Mereghetti <168123480+samuelemereghetti@users.noreply.github.com>
2024-06-01 16:42:21 +0200

    Update collatzSequence.py

--- Automate the Boring Stuff with Python/Chapter 3: Functions/collatzSequence.py
+++ Automate the Boring Stuff with Python/Chapter 3: Functions/collatzSequence.py
@@ -1,32 +1,26 @@
-# This function takes one parameter
 def collatz(number):
-    # Checks if it's even
     if number % 2 == 0:
-        # Prints result
-        print(number // 2)
-        # Returns printed value
-        return number // 2
-    # Checks if it's odd
+        result = number // 2  
     elif number % 2 == 1:
-        # Prints result
-        print(3 * number + 1)
-        # Returns printed value
-        return 3 * number + 1
+        result = 3 * number + 1
+    print(result)
+    return result
 
-# Loops and checks if user input is compatible with program
 while True:
-    # If the input is an integer try to run the Collatz sequence
     try:
-        userInput = int(input('Type an integer: '))
-        userInput = collatz(userInput)
-        while userInput != 1:
-            userInput = collatz(userInput)
+        user_input = int(input('Type an integer: '))
+        if user_input <= 0:
+            print('Please enter a positive integer.')
+            continue
         break
-
-    # If the input is not an integer handle error exception and rerun the program
+       
     except ValueError:
         print('Error:Use only integers!')
         continue
+
+ while user_input != 1:
+            user_input = collatz(user_input)
+        break
     
     
 
