54d09299a400892a6757093a4c99712e899d83d2
tonos137 <121523591+tonos137@users.noreply.github.com>
2024-07-22 16:26:56 -0700

    fix Windowssetup.sh

--- Windowssetup.sh
+++ Windowssetup.sh
@@ -1,9 +1,7 @@
 pip install virtualenv
-
-cd https://github.com/samuelemereghetti/ChessEngine/
-
+git clone https://github.com/samuelemereghetti/ChessEngine/
+cd ChessEngine
+https://github.com/samuelemereghetti/ChessEngine/
 virtualenv venv
-
 venv\Scripts\activate
-
 pip install pygame
